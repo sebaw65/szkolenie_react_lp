6cec136bb7aac9903b28863ca751c6006d81d56a
sebaw65 <sebaw65@gmail.com>
2023-02-07 20:21:25 +0100

    dodanie glownych stron i headera

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import NavigationLinks from './components/atoms/molecules/Navigation/NavigationLinks/NavigationLinks';
+
+import HomePage from './components/sites/HomePage/HomePage';
+import FeaturesPage from './components/sites/FeaturesPage/FeaturesPage';
+import TeamPage from './components/sites/TeamPage/TeamPage';
+import SigninPage from './components/sites/SigninPage/SigninPage';
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<NavigationLinks />} />
-          <Route path="/features" />
-          <Route path="/team" />
-          <Route path="/signin" />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/features" element={<FeaturesPage />} />
+          <Route path="/team" element={<TeamPage />} />
+          <Route path="/signin" element={<SigninPage />} />
         </Routes>
       </BrowserRouter>
     </div>
